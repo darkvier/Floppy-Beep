1f5558826cdf6e4dbbc2b1459885626e8772d2da
javyyk <javi.and.friends@gmail.com>
2017-04-12 17:11:56 +0200

    Settings, estetica

--- core/src/com/mygdx/game/SettingsScreen.java
+++ core/src/com/mygdx/game/SettingsScreen.java
@@ -10,16 +10,17 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Slider;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
 import static com.mygdx.game.Constants.VIEWP_MIN_SIZE;
 
 class SettingsScreen extends BaseScreen {
 
-	//TODO boton resetear config
 	private Stage stage;
 	private Skin skin;
 	private Label volText, impText, velText;
@@ -40,6 +41,52 @@ class SettingsScreen extends BaseScreen {
 		stage = new Stage(viewport);
 		skin = game.getManager().get("skin/uiskin.json");
 
+		// Fondo
+		Background backGround = new Background(game, stage, "sky/sky.png");
+		stage.addActor(backGround);
+
+		// Nombre
+		Label etiquetaNickname = new Label("Nombre Jugador: ", skin);
+		nickname = new TextField(game.nickname, skin);
+
+		// Audio
+		Label etiquetaAudio = new Label("AUDIO", skin);
+		musica = new CheckBox(" Musica", skin);
+		efectos = new CheckBox(" Efectos", skin);
+		volText = new Label("Volumen: 999%", skin);
+		volumenSlid = new Slider(0, 100, 10, false, skin);
+		volumenSlid.addListener(new ChangeListener() {
+			public void changed(ChangeEvent event, Actor actor) {
+				volText.setText("Volumen: " + volumenSlid.getValue() + "%");
+			}
+		});
+
+		// Juego
+		Label etiquetaJuego = new Label("JUEGO", skin);
+		impText = new Label("Impulso: 999%", skin);
+		impulsoSlid = new Slider(10, 30, 5, false, skin);
+		impulsoSlid.addListener(new ChangeListener() {
+			public void changed(ChangeEvent event, Actor actor) {
+				impText.setText("Impulso: " + impulsoSlid.getValue());
+			}
+		});
+
+		velText = new Label("Velocidad: 999%", skin);
+		velocidadSlid = new Slider(1, 8, 1, false, skin);
+		velocidadSlid.addListener(new ChangeListener() {
+			public void changed(ChangeEvent event, Actor actor) {
+				velText.setText("Velocidad: " + velocidadSlid.getValue());
+			}
+		});
+
+		fullScreenCheck = new CheckBox("Full Screen", skin);
+
+		// DIFICULTAD
+		Label etiquetaDificultad = new Label("Dificultad:", skin);
+		dificultad = new SelectBox<Object>(skin);
+		dificultad.setItems("Facil", "Normal", "Dificil");
+
+		// Botones
 		TextButton save = new TextButton("Save", skin);
 		save.addCaptureListener(new ChangeListener() {
 			@Override
@@ -58,97 +105,48 @@ class SettingsScreen extends BaseScreen {
 		});
 
 
-		// Nombre
-		Label etiquetaNickname = new Label("Nombre Jugador: ", skin);
-		nickname = new TextField(game.nickname, skin);
+		/* Tabla principal */
+		Table tabla = new Table();
+		//tabla.setDebug(true);
+		tabla.pad(50);
+		int padT = 50,
+				padR = 100;
+		tabla.defaults().padTop(15).expandX().align(Align.left);
+		tabla.setSize(stage.getWidth(), stage.getHeight());
 
+		tabla.row().pad(0);
+		tabla.add(etiquetaNickname).colspan(2).align(Align.right);
+		tabla.add(nickname).colspan(2).align(Align.left).minSize(300, 50);
 
-		// Audio
-		Label etiquetaAudio = new Label("AUDIO", skin);
-		musica = new CheckBox(" Musica", skin);
-		efectos = new CheckBox(" Efectos", skin);
-		volText = new Label("Volumen", skin);
-		volumenSlid = new Slider(0, 100, 10, false, skin);
-		volumenSlid.addListener(new ChangeListener() {
-			public void changed(ChangeEvent event, Actor actor) {
-				volText.setText("Volumen: " + volumenSlid.getValue() + "%");
-			}
-		});
+		tabla.row().padTop(padT);
+		tabla.add(etiquetaAudio).colspan(2).align(Align.center).padRight(padR);
+		tabla.add(etiquetaJuego).colspan(2).align(Align.center);
 
+		tabla.row().padTop(padT / 2);
+		tabla.add(efectos).colspan(2).align(Align.left).padRight(padR);
+		tabla.add(impText).size(impText.getWidth(), impText.getHeight());
+		tabla.add(impulsoSlid).fillX();
 
-		// Juego
-		Label etiquetaJuego = new Label("JUEGO", skin);
-		impText = new Label("Impulso", skin);
-		impulsoSlid = new Slider(10, 30, 5, false, skin);
-		impulsoSlid.addListener(new ChangeListener() {
-			public void changed(ChangeEvent event, Actor actor) {
-				impText.setText("Impulso: " + impulsoSlid.getValue());
-			}
-		});
+		tabla.row().padTop(padT);
+		tabla.add(musica).colspan(2).align(Align.left).padRight(padR);
+		tabla.add(velText).size(velText.getWidth(), velText.getHeight());
+		tabla.add(velocidadSlid).fillX();
 
-		velText = new Label("Velocidad", skin);
-		velocidadSlid = new Slider(1, 8, 1, false, skin);
-		velocidadSlid.addListener(new ChangeListener() {
-			public void changed(ChangeEvent event, Actor actor) {
-				velText.setText("Velocidad: " + velocidadSlid.getValue());
-			}
-		});
+		tabla.row().padTop(padT);
+		tabla.add(volText).size(volText.getWidth(), volText.getHeight());
+		tabla.add(volumenSlid).fillX().padRight(padR);
+		tabla.add(fullScreenCheck).colspan(2).align(Align.left);
 
-		fullScreenCheck = new CheckBox("Full Screen", skin);
+		tabla.row().padTop(padT);
+		tabla.add(new Label("", skin)).colspan(2);
+		tabla.add(etiquetaDificultad).padRight(padR);
+		tabla.add(dificultad).fillX().align(Align.left);
 
+		tabla.row().padTop(padT * 1.5f).size(250, 75);
+		tabla.add(save).colspan(2).align(Align.center);
+		tabla.add(back).colspan(2).align(Align.center);
 
-		// DIFICULTAD
-		Label etiquetaDificultad = new Label("DIFICULTAD", skin);
-		dificultad = new SelectBox<Object>(skin);
-		dificultad.setItems("Facil", "Normal", "Dificil");
-
-		// Sizes & Positions
-		etiquetaNickname.setPosition(200, 500 - etiquetaNickname.getHeight());
-		nickname.setPosition(350, 500 - nickname.getHeight());
-
-		etiquetaAudio.setPosition(60, 410 - etiquetaAudio.getHeight());
-		efectos.setPosition(50, 380 - efectos.getHeight());
-		musica.setPosition(50, 360 - musica.getHeight());
-		volText.setPosition(85, 330 - volText.getHeight());
-		volumenSlid.setPosition(50, 310 - volumenSlid.getHeight());
-
-		etiquetaJuego.setPosition(300, 410 - etiquetaJuego.getHeight());
-		impText.setPosition(300, 380 - impText.getHeight());
-		impulsoSlid.setPosition(300, 360 - impulsoSlid.getHeight());
-		velText.setPosition(300, 350 - velText.getHeight());
-		velocidadSlid.setPosition(300, 330 - velocidadSlid.getHeight());
-		fullScreenCheck.setPosition(300, 310 - fullScreenCheck.getHeight());
-
-		etiquetaDificultad.setPosition(500, 410 - etiquetaDificultad.getHeight());
-		dificultad.setSize(100, 20);
-		dificultad.setPosition(500, 350);
-
-		back.setSize(200, 80);
-		back.setPosition(40, 50);
-		save.setSize(200, 80);
-		save.setPosition(500, 50);
-
-
-		// addActors
-		stage.addActor(back);
-		stage.addActor(save);
-		stage.addActor(etiquetaNickname);
-		stage.addActor(nickname);
-		stage.addActor(etiquetaAudio);
-		stage.addActor(efectos);
-		stage.addActor(musica);
-		stage.addActor(volText);
-		stage.addActor(volumenSlid);
-		stage.addActor(impText);
-		stage.addActor(impulsoSlid);
-		stage.addActor(velText);
-		stage.addActor(velocidadSlid);
-		stage.addActor(fullScreenCheck);
-		stage.addActor(etiquetaJuego);
-		stage.addActor(etiquetaDificultad);
-		stage.addActor(dificultad);
-
-
+		stage.addActor(tabla);
 	}
 
 	@Override
